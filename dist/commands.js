84782a227118c4cba01c985e28db8c2a8fae0694
Serhii Korniienko <sergkornienko95@gmail.com>
2026-01-27 09:07:04 +0200

    init

---
+++ dist/commands.js
@@ -0,0 +1,2 @@
+Office.onReady(function(){}),Office.actions.associate("action",function(e){Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,e.completed()});
+//# sourceMappingURL=commands.js.map
